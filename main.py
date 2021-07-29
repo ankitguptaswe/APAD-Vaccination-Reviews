de6eca35fca902a78d7f925a6d5211f9e2e4080b
Luiz Roma <luiz.c.roma.jr@gmail.com>
2021-07-29 16:41:21 -0500

    new branch with certain pages working with mongodb

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import datetime
 import sqlite3
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from google.auth.transport import requests
 from google.cloud import datastore
 import google.oauth2.id_token
 import random, secrets
+import pymongo
+import urllib.parse
 from werkzeug.utils import secure_filename
 import os
-import pyrebase
-
-
-## MongoDB
-
-import pymongo
-from pymongo import MongoClient
-
-
-##
 
 app = Flask(__name__)
 firebase_request_adapter = requests.Request()
 datastore_client = datastore.Client()
 app.config['UPLOAD_FOLDER'] = 'static/img/reviews'
+
+
+def setup_mongodb_session():
+    username = urllib.parse.quote_plus('admin')
+    password = urllib.parse.quote_plus('asdasd@123')
+    client = pymongo.MongoClient(
+        "mongodb+srv://" + username + ":" + password + "@cluster0.le7xd.mongodb.net/?retryWrites=true&w=majority")
+    db = client['apadgroup8']
+    return db
+
 
 def store_time(email, dt):
     entity = datastore.Entity(key=datastore_client.key('User', email, 'visit'))
@@ -40,106 +42,88 @@ def fetch_times(email, limit):
 
     return times
 
-def setup_session(storage):
-    storage.child("db/reviews.db").download("/tmp/reviews.db")
-    db = sqlite3.connect("/tmp/reviews.db")
-    return db
+def update_user_theme(user_email, themes):
+    db = setup_mongodb_session()
+    #print(user_email)
+    #print("asdasd123@gmail.com")
+    #data = db.users.find()
 
-def push_db(storage, file_path, local_path):
-    storage.child(file_path).put(local_path)
-    os.remove(local_path)
+    data = db.users.find({"email": user_email})
+    #print(data[0]['themes'])
 
-def setup_firebase():
-    config = {
-    "apiKey": "AIzaSyAZh8jqAOWD42xPU9-EBIXXytNvNrtuUBE",
-    "authDomain": "vaccination-reviews-apad.firebaseapp.com",
-    "databaseURL": "accination-reviews-apad.appspot.com",
-    "projectId": "vaccination-reviews-apad",
-    "storageBucket": "vaccination-reviews-apad.appspot.com",
-    "messagingSenderId": "831689838983",
-    "appId": "1:831689838983:web:879a4fc40ea65457ed8322",
-    "serviceAccount": "vaccination-reviews-apad-9ae5ba882019.json"
-}
-    firebase = pyrebase.initialize_app(config)
-    storage = firebase.storage()
-    auth = firebase.auth()
+    #themes = ['blabla1', 'blabla2']
 
-    return storage
+    query = { "email": user_email }
+    new_preferences = { "$set": { "themes": themes } }
+
+    db.users.update_one(query, new_preferences)
+
+    data = db.users.find({"email": user_email})
+    #print(data[0]['themes'])
+    #{'_id': ObjectId('6102d061f9a93c37284f6f5e'), 'user_token': 'abc123', 'email': 'asdasd123@gmail.com', 'themes': ['vaccine', 'pharmacy']}
 
 
-def update_db (sql, task, storage):
-    db = setup_session(storage)
-    try:
-        cur = db.cursor()
-        cur.execute(sql, task)
-        db.commit()
-        push_db(storage, "db/reviews.db", "/tmp/reviews.db")
-
-    except:
-            return 'There was an issue adding your task'
-
-@app.route('/mongodb', methods=['GET', 'POST'])
-def mongo_db():
-    client = MongoClient()
-    db = client['apad_mongodb']
-    themes = db.themes
-    users = db.users
-    reviews = db.reviews
-
-    theme1 = {"name": "vaccines",
-            "about": "take and do not cry",
-            "image": "static/img/1.jpg"}
-
-    theme2 = {"name": "hospitals",
-            "about": "go and do not cry",
-            "image": "static/img/2.jpg"}
-
-    theme3 = {"name": "pharmacy",
-            "about": "wait and do not cry",
-            "image": "static/img/3.jpg"}
-
-    review1 = {"user_id": "as89dasdas90d09a",
-                "theme": "vaccine",
-                "photo": "uploaded_photo.jpg",
-                "title": "This vaccine SUCKS !",
-                "description": "I took the CROCODILE vaccine and I cant stop swimming anymore",
-                "rating": 3,
-                "tags": ["crocodile", "vaccine", "sucks"]}
-
-    user1 = {"user_id": "as89dasdas90d09a",
-            "themes": ["vaccines", "pharmacies"],
-            "email": "asdasd@gmail.com"}
-
-    #new_themes = themes.insert_many([theme1, theme2, theme3])
-    #new_review = reviews.insert_one(review1)
-    #new_user = users.insert_one(user1)
-
-    collections = db.list_collection_names()
-
-    print("BLABLABLABLALBLABLALLBLA")
-    print(collections)
-
-    #themes.drop()
-
-    if request.method == 'POST':
-        th_name = request.form['th_name']
-        th_description = request.form['th_description']
-        th_picture = request.files['photo']
-
-        theme4 = {"name": th_name,
-                "about": th_description}
-
-        themes.insert_one(theme4)
-
-        #delete_themes = request.form.getlist("aloha2")
-
-        #print("BLABLABLA")
-        #print(delete_themes)
-
-    data = themes.find()
-
-    return render_template("mongo_edit_themes.html", themes=data)
-
+# @app.route('/mongodb', methods=['GET', 'POST'])
+# def mongo_db():
+#     client = pymongo.MongoClient()
+#     db = client['apad_mongodb']
+#     themes = db.themes
+#     users = db.users
+#     reviews = db.reviews
+#
+#     theme1 = {"name": "vaccines",
+#             "about": "take and do not cry",
+#             "image": "static/img/1.jpg"}
+#
+#     theme2 = {"name": "hospitals",
+#             "about": "go and do not cry",
+#             "image": "static/img/2.jpg"}
+#
+#     theme3 = {"name": "pharmacy",
+#             "about": "wait and do not cry",
+#             "image": "static/img/3.jpg"}
+#
+#     review1 = {"user_id": "as89dasdas90d09a",
+#                 "theme": "vaccine",
+#                 "photo": "uploaded_photo.jpg",
+#                 "title": "This vaccine SUCKS !",
+#                 "description": "I took the CROCODILE vaccine and I cant stop swimming anymore",
+#                 "rating": 3,
+#                 "tags": ["crocodile", "vaccine", "sucks"]}
+#
+#     user1 = {"user_id": "as89dasdas90d09a",
+#             "themes": ["vaccines", "pharmacies"],
+#             "email": "asdasd@gmail.com"}
+#
+#     #new_themes = themes.insert_many([theme1, theme2, theme3])
+#     #new_review = reviews.insert_one(review1)
+#     #new_user = users.insert_one(user1)
+#
+#     collections = db.list_collection_names()
+#
+#     print("BLABLABLABLALBLABLALLBLA")
+#     print(collections)
+#
+#     #themes.drop()
+#
+#     if request.method == 'POST':
+#         th_name = request.form['th_name']
+#         th_description = request.form['th_description']
+#         th_picture = request.files['photo']
+#
+#         theme4 = {"name": th_name,
+#                 "about": th_description}
+#
+#         themes.insert_one(theme4)
+#
+#         #delete_themes = request.form.getlist("aloha2")
+#
+#         #print("BLABLABLA")
+#         #print(delete_themes)
+#
+#     data = themes.find()
+#
+#     return render_template("mongo_edit_themes.html", themes=data)
 
 @app.route('/')
 def root():
@@ -167,26 +151,235 @@ def root():
             token_expired = 1
 
     if not token_expired and id_token:
+
         user_id = claims['user_id']
         user_email = claims['email']
+        db = setup_mongodb_session()
 
-        file_path = "db/reviews.db"
-        local_path = "/tmp/reviews.db"
-        storage = setup_firebase()
-        db = setup_session(storage)
-        cur = db.cursor()
-        query = "SELECT * from USER WHERE USER_ID=\"" + claims['user_id'] + "\""
+        # find if user_id is present in users table
+        if db.users.find({"user_token": user_id }).count() == 0:
+            # Push user_id and user_email to db
+            db.users.insert({
+                "user_token":  user_id,
+                "email": user_email,
+                "themes": []
+            })
 
-        try:
-            cur.execute(query)
-        except sqlite3.Error as er:
-            print('SQLite error: %s' % (' '.join(er.args)))
-        data = cur.fetchall()
-
-        return render_template('index.html', user_data=claims, error_message=error_message, user = data)
+        curr = db.users.find({"user_token": user_id})
+        data = [cur for cur in curr]
+        print(data[0])
+        return render_template('index.html', user_data=claims, error_message=error_message, user=data[0])
 
     else:
         return render_template('index.html')
+
+@app.route('/preferences/set', methods=['GET', 'POST'])
+def preferences_set():
+    db = setup_mongodb_session()
+    collections = db.list_collection_names()
+    data = db.themes.find()
+
+    user_id = "abc123"
+    all_themes = []
+
+    for theme in data:
+        all_themes.append(theme['theme_name'])
+
+    details = {'email':"asdasd123@gmail.com", 'themes':["asdasd", "dsadsa"]}
+
+    if request.method == 'POST':
+        new_preferences = request.form.getlist("th_preferences")
+        update_user_theme(details['email'], new_preferences)
+
+    return render_template("preferences_set.html", details=details, all_themes=all_themes)
+
+@app.route('/preferences', methods=['GET', 'POST'])
+def preferences_show():
+    db = setup_mongodb_session()
+
+    collections = db.list_collection_names()
+    data = db.users.find()
+
+    user_id = "abc123"
+
+    #find if user_id is present in users table
+    if db.users.find({"user_token": user_id }).count() == 0:
+        # Push user_id and user_email to db
+        return redirect(url_for("preferences_set"))
+    else:
+        details = db.users.find({"user_token": user_id })[0]
+        return render_template("preferences.html", details=details)
+
+@app.route('/themes/create', methods=['GET', 'POST'])
+def create_theme():
+    if request.method == 'POST':
+        theme_name = request.form['th_name']
+        theme_description = request.form['th_description']
+        theme_photo = request.files['photo']
+
+        #'_id': ObjectId('6102cf1af9a93c37284f6f5c'), 'theme_name': 'vaccine', 'picture': b'NA', 'description': 'take it and enjoy'}
+        db = setup_mongodb_session()
+
+        collections = db.list_collection_names()
+        data = db.themes.find()
+
+        file_id = secrets.token_hex(16)
+        file_name = file_id + ".jpg"
+
+        if theme_photo.filename != '':
+            theme_photo.save("static/img/themes/" + file_name)
+
+        new_theme = {'_id': file_id,
+                    'theme_name': theme_name,
+                    'picture': file_name,
+                    'description': theme_description}
+
+        db.themes.insert_one(new_theme)
+
+    return render_template("themes_create.html")
+
+@app.route('/themes/all', methods=['GET'])
+def view_themes():
+    if request.method == 'GET':
+        #'_id': ObjectId('6102cf1af9a93c37284f6f5c'), 'theme_name': 'vaccine', 'picture': b'NA', 'description': 'take it and enjoy'}
+        db = setup_mongodb_session()
+
+        collections = db.list_collection_names()
+        data = db.themes.find()
+
+    return render_template("themes_all.html", themes_data=data)
+
+@app.route('/themes/<string:theme_name>', methods=['GET'])
+def view_theme(theme_name):
+    """
+    This function/service is used to get a single theme from the db and all associated reviews
+    :return: string containing the theme
+    """
+    db = setup_mongodb_session()
+
+    all_reviews = []
+
+    print(theme_name)
+
+    data = db.themes.find({"theme_name": theme_name})
+    data1 = db.reviews.find({"theme": theme_name})
+
+    print(data[0])
+
+    for i in data1:
+        all_reviews.append(i)
+
+
+    return render_template("theme.html", details=data[0], details1=all_reviews)
+
+# @app.route('/user/preferences', methods=['GET', 'POST'])
+# def mongo_db():
+    # Verify Firebase auth.
+    # id_token = request.cookies.get("token")
+    # error_message = None
+    # claims = None
+    # times = None
+    # token_expired = 0
+    #
+    # if id_token:
+    #     try:
+    #         # Verify the token against the Firebase Auth API. This example
+    #         # verifies the token on each page load. For improved performance,
+    #         # some applications may wish to cache results in an encrypted
+    #         # session store (see for instance
+    #         # http://flask.pocoo.org/docs/1.0/quickstart/#sessions).
+    #         claims = google.oauth2.id_token.verify_firebase_token(
+    #             id_token, firebase_request_adapter)
+    #
+    #     except ValueError as exc:
+    #         # This will be raised if the token is expired or any other
+    #         # verification checks fail.
+    #         error_message = str(exc)
+    #         token_expired = 1
+    #
+    # if not token_expired and id_token:
+    #
+    #     user_id = claims['user_id']
+    #     user_email = claims['email']
+    #     db = setup_mongodb_session()
+    #
+    #     # find if user_id is present in users table
+    #     if db.users.find({"user_token": user_id }).count() == 0:
+    #         # Push user_id and user_email to db
+    #         db.users.insert({
+    #             "user_token":  user_id,
+    #             "email": user_email,
+    #             "themes": []
+    #         })
+    #
+    #     curr = db.users.find({"user_token": user_id})
+    #     data = [cur for cur in curr]
+    #     print(data[0])
+    #     return render_template('preferences.html', user_data=claims, error_message=error_message, user=data[0])
+    # else:
+    #     return render_template('index.html')
+
+    #
+    #
+    # client = pymongo.MongoClient()
+    # db = client['apad_mongodb']
+    # themes = db.themes
+    # users = db.users
+    # reviews = db.reviews
+    #
+    # theme1 = {"name": "vaccines",
+    #         "about": "take and do not cry",
+    #         "image": "static/img/1.jpg"}
+    #
+    # theme2 = {"name": "hospitals",
+    #         "about": "go and do not cry",
+    #         "image": "static/img/2.jpg"}
+    #
+    # theme3 = {"name": "pharmacy",
+    #         "about": "wait and do not cry",
+    #         "image": "static/img/3.jpg"}
+    #
+    # review1 = {"user_id": "as89dasdas90d09a",
+    #             "theme": "vaccine",
+    #             "photo": "uploaded_photo.jpg",
+    #             "title": "This vaccine SUCKS !",
+    #             "description": "I took the CROCODILE vaccine and I cant stop swimming anymore",
+    #             "rating": 3,
+    #             "tags": ["crocodile", "vaccine", "sucks"]}
+    #
+    # user1 = {"user_id": "as89dasdas90d09a",
+    #         "themes": ["vaccines", "pharmacies"],
+    #         "email": "asdasd@gmail.com"}
+    #
+    # #new_themes = themes.insert_many([theme1, theme2, theme3])
+    # #new_review = reviews.insert_one(review1)
+    # #new_user = users.insert_one(user1)
+    #
+    # collections = db.list_collection_names()
+    #
+    # print("BLABLABLABLALBLABLALLBLA")
+    # print(collections)
+    #
+    # #themes.drop()
+    #
+    # if request.method == 'POST':
+    #     th_name = request.form['th_name']
+    #     th_description = request.form['th_description']
+    #     th_picture = request.files['photo']
+    #
+    #     theme4 = {"name": th_name,
+    #             "about": th_description}
+    #
+    #     themes.insert_one(theme4)
+    #
+    #     #delete_themes = request.form.getlist("aloha2")
+    #
+    #     #print("BLABLABLA")
+    #     #print(delete_themes)
+    #
+    # data = themes.find()
+    #
+    # return render_template("mongo_edit_themes.html", themes=data)
 
 @app.route('/review', methods=['GET'])
 def post_review():
@@ -205,8 +398,6 @@ def post_review():
         print('SQLite error: %s' % (' '.join(er.args)))
     data = cur.fetchall()
     return render_template("create_review.html", th_themes=data)
-
-
 
 @app.route('/post/review', methods=['GET', 'POST'])
 def post_review_to_db():
@@ -261,58 +452,6 @@ def post_review_to_db():
     else:
         return render_template('create_review.html')
 
-@app.route('/themes/all', methods=['GET'])
-def view_themes():
-    """
-    This function/service is used to get all themes from the db
-    :return: string containing all themes
-    """
-    storage = setup_firebase()
-    db = setup_session(storage)
-    cur = db.cursor()
-    query = "SELECT * from THEMES"
-    try:
-        cur.execute(query)
-    except sqlite3.Error as er:
-        print('SQLite error: %s' % (' '.join(er.args)))
-    data = cur.fetchall()
-    return render_template("themes.html", details=data)
-
-@app.route('/themes/<string:theme_name>', methods=['GET'])
-def view_theme(theme_name):
-    """
-    This function/service is used to get a single theme from the db and all associated reviews
-    :return: string containing the theme
-    """
-    storage = setup_firebase()
-    db = setup_session(storage)
-    cur = db.cursor()
-    query = "SELECT * FROM 'THEMES' WHERE THEME_NAME = '" + theme_name + "'"
-    try:
-        cur.execute(query)
-    except sqlite3.Error as er:
-        print('SQLite error: %s' % (' '.join(er.args)))
-    data = cur.fetchall()
-
-    query = "SELECT * FROM 'REVIEWS' WHERE THEME = '" + theme_name + "'"
-
-    try:
-        cur.execute(query)
-
-    except sqlite3.Error as er:
-        print('SQLite error: %s' % (' '.join(er.args)))
-
-    data1 = cur.fetchall()
-    list1 = []
-    list2 = []
-    for i in data1:
-        for x in i:
-            list1.append(str(x))
-        list2.append(list1)
-        list1 = []
-
-    return render_template("theme.html", details=data, details1=list2)
-
 @app.route('/reports', methods=['GET'])
 def get_reports_from_tags():
     """
@@ -335,47 +474,6 @@ def get_reports_from_tags():
         print('SQLite error: %s' % (' '.join(er.args)))
     data = cur.fetchall()
     return str(data)
-
-@app.route('/themes/create', methods=['GET', 'POST'])
-def create_theme():
-    if request.method == 'POST':
-        id_token = request.cookies.get("token")
-        error_message = None
-        claims = None
-        times = None
-
-        if id_token:
-            try:
-                claims = google.oauth2.id_token.verify_firebase_token(
-                    id_token, firebase_request_adapter)
-
-            except ValueError as exc:
-                error_message = str(exc)
-
-        th_name = request.form['th_name']
-        th_description = request.form['th_description']
-        th_photo = request.files['photo']
-
-        file_path = os.path.join('static/img/themes', th_name)
-        local_path = os.path.join('/tmp', th_name)
-        th_photo.save(local_path)
-
-        storage = setup_firebase()
-        db = setup_session(storage)
-        sql = ''' INSERT INTO THEMES(THEME_NAME,PICTURE,DESCRIPTION)
-                  VALUES(?,?,?) '''
-        task = (th_name,th_name,th_description)
-        try:
-            cur = db.cursor()
-            cur.execute(sql, task)
-            db.commit()
-            push_db(storage, file_path, local_path)
-            push_db(storage, "db/reviews.db", "/tmp/reviews.db")
-            return view_themes()
-        except:
-            return 'There was an issue adding your task'
-    else:
-        return render_template('create_theme.html')
 
 @app.route('/user/page/set', methods=['GET', 'POST'])
 def set_preferences():
