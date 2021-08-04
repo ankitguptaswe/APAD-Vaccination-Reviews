554c71aae7b99439f7563925c13a0527d8f29aff
Ankit Gupta <ankitgupta@Ankits-MacBook-Air.local>
2021-08-05 01:35:12 +0530

    cleanup folders/file which are redundant

--- abc.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-import urllib.parse
-username = urllib.parse.quote_plus('admin')
-password = urllib.parse.quote_plus('asdasd@123')
-client = pymongo.MongoClient("mongodb+srv://" + username + ":" + password+ "@cluster0.le7xd.mongodb.net/?retryWrites=true&w=majority")
-db = client['apadgroup8']
-print(db.list_collection_names())
